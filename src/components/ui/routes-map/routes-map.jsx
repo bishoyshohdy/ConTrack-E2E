ceb832738b82131f42da3758893c8b4c18a1e890
karol adel <karoladelk@gmail.com>
2023-09-11 11:50:16 +0300

    10-9 (cylock page)

--- src/components/ui/routes-map/routes-map.jsx
+++ src/components/ui/routes-map/routes-map.jsx
@@ -89,6 +89,99 @@ function RoutesMap ({ markers = [], routes = [], geofences = [], setRoute = fals
         visible: true,
         radius: 300000
     };
+
+    const darkMapStyle = [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }],
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }],
+        },
+        {
+            zoomControl: true,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: true
+        }
+      ];
+    
+    
+      const mapOptions = {
+        styles: darkMapStyle, // Set the dark map style here
+      };
+
     return (
         <>
             <Flex
@@ -158,16 +251,12 @@ function RoutesMap ({ markers = [], routes = [], geofences = [], setRoute = fals
                 }
                 <Box h="100%" w="100%">
                     {/* Google Map Box */}
-                    {deviceCtx && deviceCtx.isLoaded && <GoogleMap
+                    {deviceCtx && deviceCtx.isLoaded && 
+                    <GoogleMap
                         center={center}
                         zoom={zoom}
                         mapContainerStyle={{ minHeight: '500px', width: '100%', height: '90%' }}
-                        options={{
-                            zoomControl: true,
-                            streetViewControl: false,
-                            mapTypeControl: false,
-                            fullscreenControl: true
-                        }}
+                        options={mapOptions}
                         onLoad={(map) => setMap(map)}
                     >
                         {
